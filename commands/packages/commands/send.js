940dc8cc0da890ea022ee968d80ae7d894a2465a
briancreate <brianredirect4455@gmail.com>
2026-01-04 18:01:26 +0100

    [ADD] Command Structure For Publish System

--- commands/packages/commands/send.js
+++ commands/packages/commands/send.js
@@ -1,10 +1,39 @@
 module.exports = {
   data: new SlashCommandSubcommandBuilder()
     .setName("send")
-    .setDescription("‎ "),
+    .setDescription("‎ ")
+    .addStringOption((option) =>
+      option
+        .setName('package')
+        .setDescription("ID of the package you are attempting to publish")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('forum')
+        .setDescription("The forum you want to publish the package in")
+        .setRequired(true)
+        .addChoices(
+          { name: "Liveries Forum", value: "liveries" },
+          { name: "Clothing Forum", value: "clothing" },
+          { name: "Bot Code Forum", value: "code" },
+        )
+    )
+    .addAttachmentOption((option) =>
+      option
+        .setName('image')
+        .setDescription("Attach a product image/ video for the package")
+        .setRequired(true)
+    )
+    .addAttachmentOption((option) =>
+      option
+        .setName('file')
+        .setDescription("Attach the delivery file for the package")
+        .setRequired(true)
+    )
 
   async execute(interaction) {
-
+    s
 
   }
 };
