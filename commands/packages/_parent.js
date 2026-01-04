6d953610dacc58e9e7d66b741243e62a3e8d967d
briancreate <brianredirect4455@gmail.com>
2026-01-04 15:55:41 +0100

    [ADD] Implement Sub-Command Handler

---
+++ commands/packages/_parent.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  data: (() => {
+    const builder = new SlashCommandBuilder()
+      .setName("giveaway")
+      .setDescription("Giveaway..")
+      .setDefaultMemberPermissions(PermissionFlagsBits.Administrator);
+
+    const subFolder = path.join(__dirname, "commands");
+    const subFiles = fs.readdirSync(subFolder).filter(f => f.endsWith(".js"));
+
+    for (const file of subFiles) {
+      const sub = require(path.join(subFolder, file));
+      builder.addSubcommand(sub.data); // Assuming sub.data is a builder instance
+    }
+
+    return builder;
+  })(),
+
+  async execute(interaction) {
+    const sub = interaction.options.getSubcommand();
+    const handler = require(`./commands/${sub}.js`);
+    return handler.execute(interaction);
+  }
+};
